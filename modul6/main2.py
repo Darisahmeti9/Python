9010175a529532769012da202cb7cc871d20b738
darisahmeti9 <Student@DIGITALSCHOOL.LOCAL>
2025-11-20 16:03:08 +0100

    modul5

---
+++ modul6/main2.py
@@ -0,0 +1,13 @@
+# import math as m
+#
+# result=m.sqrt(25)
+# print(result)
+
+from math import sqrt as sq
+
+from math import sqrt as sq
+
+from modul6.main import result
+
+result=sq(25)
+print(result)
